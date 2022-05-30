6a22db2c5c87156fc794c8a86170639699cc4992
luabida <luabidaa@gmail.com>
2022-05-30 16:12:29 -0300

    Set Jupyter-book to work properly

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,22 +5,31 @@ import subprocess
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 {% if cookiecutter.documentation_engine != 'Sphinx' %}
-from pathlib import Path
-import glob
-DOCS_FILES_PATH = Path(PROJECT_DIRECTORY) / 'docs' / '*'
-DOCS_FILES = glob.glob(str(DOCS_FILES_PATH))
 
-def remove_docs():
-    for f in DOCS_FILES: os.remove(f)
+def remove_docs_files():
+    DOCS_PATH = f'{PROJECT_DIRECTORY}/docs'
+    for file in os.listdir(DOCS_PATH):
+        file_path = os.path.join(DOCS_PATH, file)
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            pass
 {% endif %}
 
 {% if cookiecutter.documentation_engine != 'mkdocs' %}
 from pathlib import Path
-import glob
 MKDOCS_FILE = Path(PROJECT_DIRECTORY) / 'mkdocs.yaml'
 
 def remove_mkdocs_yaml():
     os.remove(MKDOCS_FILE)
+{% endif %}
+
+{% if cookiecutter.documentation_engine != 'Jupyter-book' %}
+import shutil
+
+def remove_book():
+    BOOK_PATH = f'{PROJECT_DIRECTORY}/docs/{{cookiecutter.project_slug}}-book'
+    shutil.rmtree(BOOK_PATH)
 {% endif %}
 
 def remove_file(filepath):
@@ -33,6 +42,18 @@ def http2ssh(url):
 
 
 def post_gen():
+    {% if cookiecutter.documentation_engine != 'Sphinx' %}
+    remove_docs_files()
+    {% endif %}
+
+    {% if cookiecutter.documentation_engine != 'mkdocs' %}
+    remove_mkdocs_yaml()
+    {% endif %}
+
+    {% if cookiecutter.documentation_engine != 'Jupyter-book' %}
+    remove_book()
+    {% endif %}
+
     subprocess.call(["git", "init"])
 
     git_https_origin = http2ssh("{{cookiecutter.git_https_origin}}")
@@ -61,13 +82,7 @@ def post_gen():
     subprocess.call(["git", "add", "."])
     subprocess.call(["git", "commit", "-m", "Initial commit"])
 
-    {% if cookiecutter.documentation_engine != 'Sphinx' %}
-    remove_docs()
-    {% endif %}
 
-    {% if cookiecutter.documentation_engine != 'mkdocs' %}
-    remove_mkdocs_yaml()
-    {% endif %}
 
 if __name__ == "__main__":
     post_gen()
