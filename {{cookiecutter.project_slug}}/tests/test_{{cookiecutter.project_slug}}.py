145dc89e8614e594c2e5758f26b0ac145f00c0f8
luabida <82233055+luabida@users.noreply.github.com>
2022-06-03 10:47:24 -0300

    feat: Add options for documentation and fix bugs (#5)
    * fixing missing input and git_remote_upstream attr
    
    * remove duplicated ccutter inputs
    
    * 2 pkg-dependencies pinned to older versions
    
    * changing project_url to pass github ci tests
    
    * applying generic github urls to force mkdocs to pass
    
    * remove undefined make command from template (make test-template)
    
    * Creating poll between Sphinx and mkdocs. Jupyter-book is missing config.
    
    * Improve poll of doc generators and Makefile
    
    * Set Jupyter-book to work properly
    
    * Fix mkdocs breaking on default template
    
    * Separate each Doc. Gen. in a specific dir
    
    * Remove unnecessary files from Jupyter-book
    
    * Fix package conflicts myst-parser
    
    * Implement a name changer for Docs. Gen. directory

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from {{ cookiecutter.package_slug }} import {{ cookiecutter.package_slug }}
+#from {{cookiecutter.package_slug}} import {{ cookiecutter.package_slug }}
 
 
 @pytest.fixture
@@ -11,8 +11,8 @@ def response():
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
-    # import requests
-    # return requests.get('https://github.com/osl-incubator/cookiecutter-python')
+# import requests
+# return requests.get('https://github.com/osl-incubator/cookiecutter-python')
 
 
 def test_content(response):
