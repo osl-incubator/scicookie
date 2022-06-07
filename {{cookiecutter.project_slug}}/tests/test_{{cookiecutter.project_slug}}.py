2ac0d32d19759d9e8051bf084b7c3b3de53cd790
luabida <luabidaa@gmail.com>
2022-06-07 14:45:39 -0300

    Remove former code from template test that was causing bugs

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -2,20 +2,12 @@
 
 import pytest
 
-#from {{cookiecutter.package_slug}} import {{ cookiecutter.package_slug }}
-
-
 @pytest.fixture
 def response():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
-# import requests
-# return requests.get('https://github.com/osl-incubator/cookiecutter-python')
-
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
