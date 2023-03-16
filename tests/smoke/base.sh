887dfa6b56ca4068a15ac191b7e03bfe58e04fa3
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-03-16 11:24:33 -0400

    tests(smoke): Fix the path used in the smoke tests base script (#64)

--- tests/smoke/base.sh
+++ tests/smoke/base.sh
@@ -4,6 +4,7 @@ set -e
 
 PATH_ORI=${PATH}
 CONDA_PATH=$(cd $(dirname $(which conda)) && cd .. && pwd)
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd ../.. && pwd )"
 
 export PATH="${CONDA_PATH}/condabin:${CONDA_PATH}/bin:$PATH"
 echo "[II] included conda to the PATH"
@@ -12,7 +13,7 @@ ENV_NAME=osl-python-package
 
 rm -rf "/tmp/${ENV_NAME}"
 
-cookiecutter --output-dir /tmp/ --no-input . ${1}="${2}"
+cookiecutter --output-dir /tmp/ --no-input ${PROJECT_PATH} ${1}="${2}"
 cd "/tmp/${ENV_NAME}"
 
 mamba env create --file conda/dev.yaml --force
