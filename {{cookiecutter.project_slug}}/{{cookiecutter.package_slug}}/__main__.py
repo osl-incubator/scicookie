039018d1360075e26d0f05883d0c8be773f4a0ae
Anavelyz <anavelyz@gmail.com>
2023-04-26 20:04:40 -0400

    update main

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__main__.py
@@ -1,18 +1,18 @@
 """
-Entrypoint module, in case you use `python -m{{cookiecutter.project_name}}`.
+Entrypoint module, in case you use `python -m{{cookiecutter.project_slug}}`.
 Why does this file exist, and why __main__? For more info, read:
 - https://www.python.org/dev/peps/pep-0338/
 - https://docs.python.org/2/using/cmdline.html#cmdoption-m
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
-{%- if cookiecutter.command_line_interface == 'Click' or cookiecutter.command_line_inteface == 'Argparse' %}
+ {%- if cookiecutter.command_line_interface is in ["Click", "Argparse"] %}
 import sys
 
 {% endif %}
-from {{cookiecutter.package_slug}}.cli import main
+from {{cookiecutter.project_slug}}.cli import main
 
 if __name__ == "__main__":
-{%- if cookiecutter.command_line_interface == 'Click' or cookiecutter.command_line_inteface == 'Argparse' %}
+ {%- if cookiecutter.command_line_interface is in ["Click", "Argparse"] %}
     sys.exit(main())
 {%- else %}
     main()
