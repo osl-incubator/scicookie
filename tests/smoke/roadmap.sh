980293768bac4d55b250028d233b2b22ca268f49
Yurely <yurely.camacho@gmail.com>
2023-05-04 18:16:21 -0400

    Add tests for roadmap

---
+++ tests/smoke/roadmap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+. ${SMOKE_DIR}/base.sh roadmap "PyTorch-Ignite roadmap document"
+. ${SMOKE_DIR}/base.sh roadmap "None"
