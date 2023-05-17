8514ef240c0be9942096af010d5a504c41695fa1
Anavelyz <anavelyz@gmail.com>
2023-05-17 13:58:29 -0400

    Fix mypy error

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__main__.py
@@ -1,18 +1,17 @@
 """
-Entrypoint module, in case you use `python -m{{cookiecutter.project_slug}}`.
+Entrypoint module, in case you use `python -m {{cookiecutter.project_slug}}`.
 Why does this file exist, and why __main__? For more info, read:
 - https://www.python.org/dev/peps/pep-0338/
 - https://docs.python.org/2/using/cmdline.html#cmdoption-m
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
- {%- if cookiecutter.command_line_interface is in ["Click", "Argparse"] %}
+{%- if cookiecutter.command_line_interface is in ["Click", "Argparse"] %}
 import sys
-
 {% endif %}
-from {{cookiecutter.package_slug}}.cli import main
+from {{cookiecutter.package_slug}}.cli import main  # type: ignore
 
 if __name__ == "__main__":
- {%- if cookiecutter.command_line_interface is in ["Click", "Argparse"] %}
+{%- if cookiecutter.command_line_interface is in ["Click", "Argparse"] %}
     sys.exit(main())
 {%- else %}
     main()
