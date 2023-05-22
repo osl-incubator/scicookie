e88a9a7a914e5fb78bb78e048cffbb3bc94247ad
Yurely <yurely.camacho@gmail.com>
2023-05-22 16:48:22 -0400

    fix error in pydocstyle and blue hooks

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__main__.py
@@ -1,10 +1,12 @@
 """
 Entrypoint module, in case you use `python -m {{ cookiecutter.project_slug }}`.
+
 Why does this file exist, and why __main__? For more info, read:
 - https://www.python.org/dev/peps/pep-0338/
 - https://docs.python.org/2/using/cmdline.html#cmdoption-m
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
+
 {%- if cookiecutter.auto_format_tool == 'blue' %}
   {%- set QUOTE = "'" -%}
 {%- elif cookiecutter.auto_format_tool == 'black' %}
@@ -17,6 +19,7 @@ Why does this file exist, and why __main__? For more info, read:
 import sys
 
 {% endif -%}
+
 from {{cookiecutter.package_slug}}.cli import main  # type: ignore
 
 if __name__ == {{ QUOTE }}__main__{{ QUOTE }}:
