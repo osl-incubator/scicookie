4a5c849f40548f03494aab80418e596af624200a
ayeankit <ankitkumar36918@gmail.com>
2023-06-20 23:28:24 +0530

    Added hypothesis

--- src/scicookie/{{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ src/scicookie/{{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,4 +1,5 @@
 """Tests for `{{ cookiecutter.project_slug }}` package."""
+{%- if cookiecutter.use_pytest == "yes" -%}
 
 import pytest
 
@@ -13,3 +14,22 @@ def response():
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
+{% endif %}
+
+{%- if cookiecutter.use_hypothesis == "yes" -%}
+
+from hypothesis import given, strategies as st 
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: https://hypothesis.readthedocs.io/en/latest/quickstart.html
+    """
+
+
+@given(st.text())
+def test_content(response, hypothesis_argument):
+    """Sample pytest test function with the pytest fixture and hypothesis as arguments."""
+    # Test code using the response fixture and hypothesis_argument
+{% endif %}
