04851798b1a5735a9750ac1b662d9a277dea784b
ayeankit <ankitkumar36918@gmail.com>
2023-06-21 05:28:05 +0530

    pre-commit -verbose

--- src/scicookie/{{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ src/scicookie/{{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,36 +1,41 @@
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 {%- if cookiecutter.use_pytest == "yes" -%}
-
 import pytest
+{%- endif %}
+
+{%- if cookiecutter.use_hypothesis == "yes" -%}
+from hypothesis import given, strategies as st
+{%- endif %}
 
 
+{%- if cookiecutter.use_pytest == "yes" -%}
 @pytest.fixture
 def response():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
+{%- endif -%}
 
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-{% endif %}
 
 {%- if cookiecutter.use_hypothesis == "yes" -%}
-
-from hypothesis import given, strategies as st 
-
 @pytest.fixture
 def response():
     """Sample pytest fixture.
 
     See more at: https://hypothesis.readthedocs.io/en/latest/quickstart.html
     """
+{%- endif -%}
 
 
+{%- if cookiecutter.use_pytest == "yes" and cookiecutter.use_hypothesis == "yes" -%}
 @given(st.text())
 def test_content(response, hypothesis_argument):
     """Sample pytest test function with the pytest fixture and hypothesis as arguments."""
     # Test code using the response fixture and hypothesis_argument
-{% endif %}
 
+@given(st.text())
+def test_content(response, hypothesis_argument):
+    """Sample pytest test function with the hypothesis fixture as an argument."""
+    # Test code using the response fixture and hypothesis_argument
+{%- endif -%}
