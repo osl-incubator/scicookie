d496312966d38a8d99755940a50835a5a545a682
ayeankit <ankitkumar36918@gmail.com>
2023-06-27 04:40:41 +0530

    test file changed

--- src/scicookie/{{cookiecutter.project_slug}}/tests/test_{{cookiecutter.package_slug}}.py
+++ src/scicookie/{{cookiecutter.project_slug}}/tests/test_{{cookiecutter.package_slug}}.py
@@ -1,26 +1,24 @@
-"""Tests for `{{ cookiecutter.package_slug }}` package.
 """
-{%- if cookiecutter.use_pytest == "yes" %}
+Tests for {{ cookiecutter.package_slug }} package.
+"""
+{% if cookiecutter.use_pytest == "yes" -%}
 import pytest
 {% endif -%}
-
-{%- if cookiecutter.use_hypothesis == "yes" %}
-from hypothesis import given, strategies as st
+{% if cookiecutter.use_hypothesis == "yes" -%}
+from hypothesis import given
+from hypothesis import strategies as st
 {% endif -%}
+{% if cookiecutter.use_pytest == "yes" %}
 
-
-{%- if cookiecutter.use_pytest == "yes" %}
 @pytest.fixture
 def response_pytest():
     """Sample pytest fixture.
 
-    See more at: 
+    See more at:
     http://doc.pytest.org/en/latest/fixture.html
     """
 {% endif -%}
-
-
-{%- if cookiecutter.use_hypothesis == "yes" -%}
+{% if cookiecutter.use_hypothesis == "yes" %}
 
 @pytest.fixture
 def response_hypothesis():
@@ -30,24 +28,24 @@ def response_hypothesis():
     https://hypothesis.readthedocs.io/en/latest/quickstart.html
     """
 {% endif -%}
-
-
 {%- if cookiecutter.use_pytest == "yes" and 
-cookiecutter.use_hypothesis == "yes" -%}
+cookiecutter.use_hypothesis == "yes" %}
+
 @given(st.text())
 def test_content_hypothesis1():
-    """Sample pytest test function with the 
+    """Sample pytest test function with the
     pytest fixture and hypothesis as arguments.
     """
-    # Test code using the response fixture and 
+    # Test code using the response fixture and
     # hypothesis_argument
+
 
 @given(st.text())
 def test_content_hypothesis2():
-    """Sample pytest test function with the hypothesis 
+    """Sample pytest test function with the hypothesis
     fixture as an argument.
     """
-    # Test code using the response fixture and 
+    # Test code using the response fixture and
     # hypothesis_argument
 {% endif -%}
 {#- keep this line at the end of the file -#}
