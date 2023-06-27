44ecccf03f4c7f04f37044b4fd1a4bb3d6e0e75c
Anavelyz Perez <42752529+Anavelyz@users.noreply.github.com>
2023-06-27 13:10:55 -0400

    feat(template): Adding flit as a build-system option (#136)
    * Activate option in TUI to poetry and flit.
    * Adding documentation about flit and poetry.
    * Adding flit as a build-system:
    * Creating a flit-pyproject.toml
    * Editing post_gen_project.py
    * Creating a smoke test (build-system.sh)
    * Editing cookicutter.json

---
+++ tests/smoke/build-system.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+. ${SMOKE_DIR}/base.sh "build_system=poetry"
+. ${SMOKE_DIR}/base.sh "build_system=flit"
