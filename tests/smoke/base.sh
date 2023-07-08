06f797f8b95d058cf8fe1b94f2bba6651cbdade6
Ankit Kumar <72691866+ayeankit@users.noreply.github.com>
2023-07-08 05:57:21 +0530

    Update base.sh

--- tests/smoke/base.sh
+++ tests/smoke/base.sh
@@ -50,7 +50,7 @@ elif command -v meson &> /dev/null; then
 elif command -v pdm &> /dev/null; then
   pdm install
 elif command -v hatchling &> /dev/null; then
-  hatchling install  
+  pip install hatch  
 else
     # use setuptools
   pip install --editable .
