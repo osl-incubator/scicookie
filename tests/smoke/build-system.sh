746be9400a48329f4905b3502b7ad6a0286c5af8
ayeankit <ankitkumar36918@gmail.com>
2023-07-20 17:40:29 +0530

    added dynamic version

--- tests/smoke/build-system.sh
+++ tests/smoke/build-system.sh
@@ -2,9 +2,4 @@
 
 SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-. ${SMOKE_DIR}/base.sh "build_system=poetry"
-. ${SMOKE_DIR}/base.sh "build_system=flit"
-. ${SMOKE_DIR}/base.sh "build_system=mesonpy"
-. ${SMOKE_DIR}/base.sh "build_system=setuptools"
-. ${SMOKE_DIR}/base.sh "build_system=pdm"
 . ${SMOKE_DIR}/base.sh "build_system=hatch"
