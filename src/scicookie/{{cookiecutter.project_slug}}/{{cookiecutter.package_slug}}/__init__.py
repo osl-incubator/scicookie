b12255029a10a1c17c1e2b7af5d0006d16c9ff09
Saransh Chopra <saransh0701@gmail.com>
2023-07-27 21:44:19 -0400

    Make `mypy` optional + add `py.typed` (#158)
    - `mypy` now appears in the TUI as an option
    - `py.typed` is added by default but removed if `mypy` is not selected
    - removed the type signatures in the package codebase, to make `mypy` optional

--- src/scicookie/{{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__init__.py
+++ src/scicookie/{{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__init__.py
@@ -1,5 +1,7 @@
-# mypy: disable-error-code="attr-defined"
 """{{ cookiecutter.project_name }}."""
+{%- if cookiecutter.use_mypy == "yes" %}
+# mypy: disable-error-code="attr-defined"
+{%- endif %}
 {%- if cookiecutter.use_blue == "yes" %}
   {%- set QUOTE = "'" -%}
 {%- elif cookiecutter.use_black == "yes" %}
@@ -10,7 +12,7 @@
 from importlib import metadata as importlib_metadata
 
 
-def get_version() -> str:
+def get_version():
     """Return the program version."""
     try:
         return importlib_metadata.version(__name__)
@@ -18,8 +20,8 @@ def get_version() -> str:
         return {{ QUOTE }}{{ cookiecutter.project_version }}{{ QUOTE }}  # semantic-release
 
 
-version: str = get_version()
+version = get_version()
 
 __author__ = {{ QUOTE }}{{ cookiecutter.author_full_name }}{{ QUOTE }}
 __email__ = {{ QUOTE }}{{ cookiecutter.author_email }}{{ QUOTE }}
-__version__: str = version
+__version__ = version
