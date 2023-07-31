osl-incubator/scicookie

commit bea6ad2742decb4c05ee52478ea37386b870588b

feat: dynamic versioning for hatch (#156)
This PR switches hatch to use dynamic versioning, similar to flit and pdm in this repository. There are also some minor fixes which I found while using the repo -

- redundant noqa
- typo in workflow file

src/scicookie/{{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__init__.py
@@ -9,6 +9,9 @@
 {%- else %}
   {%- set QUOTE = "'" -%}
 {%- endif %}
+{% if cookiecutter.build_system == "hatch" -%}
+__version__ = {{ QUOTE }}{{ cookiecutter.project_version }}{{ QUOTE }}
+{%- else %}
 from importlib import metadata as importlib_metadata
 
 
@@ -22,6 +25,7 @@ def get_version():
 
 version = get_version()
 
-__author__ = {{ QUOTE }}{{ cookiecutter.author_full_name }}{{ QUOTE }}
-__email__ = {{ QUOTE }}{{ cookiecutter.author_email }}{{ QUOTE }}
 __version__ = version
+{%- endif %}
+__author__ = {{ QUOTE }}{{ cookiecutter.project_version }}{{ QUOTE }}
+__email__ = {{ QUOTE }}{{ cookiecutter.author_email }}{{ QUOTE }}

.github/workflows/main.yaml
@@ -89,5 +89,5 @@ jobs:
       - name: Install dependencies
         run: poetry install
 
-      - name: Run Smoke Test (mkdocs)
+      - name: Run Smoke Test (${{ matrix.smoke_file }})
         run: bash ./tests/smoke/${{ matrix.smoke_file }}

src/scicookie/{{cookiecutter.project_slug}}/build-system/hatch-pyproject.toml
@@ -34,7 +34,7 @@ classifiers = [
   "Topic :: Scientific/Engineering",
   "Typing :: Typed",
 ]
-version = "{{ cookiecutter.project_version }}"
+dynamic = ["version"]
 requires-python = ">=3.8.1"
 dependencies = [
 {# keep this line here #}

commit 67a40afba6d0041b226e1c37ff46f252a36f843d

fix: Fix error in `__author__` introduced by #156

src/scicookie/{{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__init__.py
@@ -27,5 +27,5 @@ version = get_version()
 
 __version__ = version
 {%- endif %}
-__author__ = {{ QUOTE }}{{ cookiecutter.project_version }}{{ QUOTE }}
+__author__ = {{ QUOTE }}{{ cookiecutter.author_full_name }}{{ QUOTE }}
 __email__ = {{ QUOTE }}{{ cookiecutter.author_email }}{{ QUOTE }}