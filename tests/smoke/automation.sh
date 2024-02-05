6498d5e65b510804ed834c55da377ca4526581a6
ayeankit <ankitkumar36918@gmail.com>
2024-02-05 22:07:56 +0530

    demo

--- tests/smoke/automation.sh
+++ tests/smoke/automation.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
 . ${SMOKE_DIR}/base.sh "use_makim=yes use_make=no"
