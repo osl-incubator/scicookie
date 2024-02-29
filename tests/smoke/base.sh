a26303185b65166b87aac6a7d483e600d6782e10
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2024-02-28 21:11:20 -0400

    fix: Fix conda activation used by the smoke tests

--- tests/smoke/base.sh
+++ tests/smoke/base.sh
@@ -4,15 +4,13 @@ set -ex
 
 PATH_ORI=${PATH}
 PWD_ORI=$(pwd)
-CONDA_PATH=$(cd "$(dirname ${CONDA_PYTHON_EXE})" && cd .. && pwd)
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" && cd ../src/scicookie && pwd )" >/dev/null 2>&1 && cd ../.. && pwd )"
 
-if [ "${CONDA_PATH}" == ""]; then
+if [ "$(which conda)}" == ""]; then
   echo "INVALID 'CONDA_PATH' environment variable."
   exit 1
 fi
 
-export PATH="${CONDA_PATH}/condabin:${CONDA_PATH}/bin:$PATH"
 echo "[II] included conda to the PATH"
 
 ENV_NAME=osl-python-package
@@ -28,22 +26,20 @@ cookieninja --no-input \
 
 cd "${OUTPUT_DIR}/${ENV_NAME}"
 
+eval "$(conda shell.bash hook)"
 mamba env create --file conda/dev.yaml --force
+conda activate "${ENV_NAME}"
 
-CONDA_PREFIX="${CONDA_PATH}/envs/${ENV_NAME}"
+# export PATH=$(echo $PATH| sed -E "s/[^:]+\/mambaforge\/[^:]+//g")
+# export PATH=$(echo $PATH| sed -E "s/[^:]+\/conda\/[^:]+//g")
+# export PATH=$(echo $PATH| sed -E "s/[^:]+\/miniconda\/[^:]+//g")
+# export PATH=$(echo $PATH| sed -E "s/[^:]+\/miniconda3\/[^:]+//g")
+# export PATH=$(echo $PATH| sed -E "s/[^:]+\/micromamba\/[^:]+//g")
+# export PATH=$(echo $PATH| sed -E "s/[^:]+\/anaconda3\/[^:]+//g")
+# export PATH="${CONDA_PREFIX}:${CONDA_PREFIX}/bin:$PATH"
+# echo "[II] included env conda to the PATH"
 
-export PATH=$(echo $PATH| sed -E "s/[^:]+\/mambaforge\/[^:]+//g")
-export PATH=$(echo $PATH| sed -E "s/[^:]+\/conda\/[^:]+//g")
-export PATH=$(echo $PATH| sed -E "s/[^:]+\/miniconda\/[^:]+//g")
-export PATH=$(echo $PATH| sed -E "s/[^:]+\/miniconda3\/[^:]+//g")
-export PATH=$(echo $PATH| sed -E "s/[^:]+\/micromamba\/[^:]+//g")
-export PATH=$(echo $PATH| sed -E "s/[^:]+\/anaconda3\/[^:]+//g")
-export PATH="${CONDA_PREFIX}:${CONDA_PREFIX}/bin:$PATH"
-echo "[II] included env conda to the PATH"
-
-if command -v poetry &> /dev/null; then
-  poetry install
-elif command -v flit &> /dev/null; then
+if command -v flit &> /dev/null; then
   flit install
 elif command -v meson &> /dev/null; then
   pip install .
@@ -58,6 +54,8 @@ elif [ "$(pip list|grep -c scikit_build_core)" -ne "0" ]; then
 elif [ "$(pip list|grep -c pybind11)" -ne "0" ]; then
   # Assuming you are inside the root of the CMake source directory
   pip install .
+elif command -v poetry &> /dev/null; then
+  poetry install
 else
   # use setuptools
   pip install .
--- tests/smoke/build-systems.sh
+++ tests/smoke/build-systems.sh
@@ -2,7 +2,6 @@
 
 SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-. ${SMOKE_DIR}/base.sh "build_system=poetry"
 . ${SMOKE_DIR}/base.sh "build_system=flit"
 . ${SMOKE_DIR}/base.sh "build_system=mesonpy"
 . ${SMOKE_DIR}/base.sh "build_system=setuptools"
@@ -11,3 +10,4 @@ SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . ${SMOKE_DIR}/base.sh "build_system=maturin"
 . ${SMOKE_DIR}/base.sh "build_system=scikit-build-core"
 . ${SMOKE_DIR}/base.sh "build_system=pybind11"
+. ${SMOKE_DIR}/base.sh "build_system=poetry"
