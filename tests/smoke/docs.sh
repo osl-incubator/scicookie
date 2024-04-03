9aed15b5cb3eba56e51b8af1934b7a9814ef4ea5
Anavelyz Perez <42752529+Anavelyz@users.noreply.github.com>
2024-04-03 11:51:29 -0400

    feat(template): Add quarto documentation engine (#257)

--- tests/smoke/docs.sh
+++ tests/smoke/docs.sh
@@ -6,3 +6,4 @@ SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . ${SMOKE_DIR}/base.sh 'documentation_engine=mkdocs'
 . ${SMOKE_DIR}/base.sh 'documentation_engine=jupyter-book'
 . ${SMOKE_DIR}/base.sh 'documentation_engine=sphinx'
+. ${SMOKE_DIR}/base.sh 'documentation_engine=quarto'
