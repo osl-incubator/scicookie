e1467c1562d93a9b32356f68186d5cd13049954a
Naman Priyadarshi <namanpriyadarshi@Namans-MacBook-Air.local>
2024-07-13 15:31:15 +0530

    added options for release_workflow

--- src/scicookie/{{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__init__.py
+++ src/scicookie/{{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/__init__.py
@@ -15,7 +15,12 @@ def get_version() -> str:
     try:
         return importlib_metadata.version(__name__)
     except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        {%- if cookiecutter.release_worlflow == "semantic-release" %}
         return {{ QUOTE }}{{ cookiecutter.project_version }}{{ QUOTE }}  # semantic-release
+        {%- endif %}
+        {%- if cookiecutter.release_worlflow == "release-please" %}
+        return {{ QUOTE }}{{ cookiecutter.project_version }}{{ QUOTE }}  # release-please
+        {%- endif %}
 
 
 version = get_version()
