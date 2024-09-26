859bfe0ae6cd2b21636ea5145dfc486c770b0af1
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2024-09-26 15:36:56 -0400

    test: Add smoke test for pyenv and split base.sh into base and base-template scripts (#315)

--- tests/smoke/base.sh
+++ tests/smoke/base.sh
@@ -1,57 +1,15 @@
 #!/usr/bin/env bash
 echo "-------------------- Smoke test for ${1} -------------------"
+
+export PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd ../.. && pwd )"
+
+source $PROJECT_PATH/tests/smoke/base-template.sh ${1}
+
 set -ex
-
-PATH_ORI=${PATH}
-PWD_ORI=$(pwd)
-PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd ../.. && pwd )"
-
-CONDA_PATH=$(ensureconda --conda --no-install | sed 's:/mamba$:/conda:')
-MAMBA_PATH=$(ensureconda --mamba --no-install | sed 's:/conda$:/mamba:')
-
-if [[ ! -f "$MAMBA_PATH" ]]; then
-  echo "[EE] 'mamba' not found."
-  exit 1
-fi
-
-if [[ ! -f "$CONDA_PATH" ]]; then
-  echo "[EE] 'conda' not found."
-  exit 1
-fi
-
-input_params="$1"
-
-if [[ "${input_params}" == *"use_conda=yes"* ]] || [[ "$input_params" == *"use_pyenv=yes"* ]]; then
-  echo "Virtual environment defined."
-else
-  input_params="use_conda=yes ${input_params}"
-fi
-
-if [[ "${input_params}" == *"use_makim=yes"* ]] || [[ "$input_params" == *"use_make=yes"* ]]; then
-  echo "Automation task tool defined."
-else
-  input_params="use_makim=yes ${input_params}"
-fi
-
-if [[ "${input_params}" == *"use_pre_commit=yes"* ]]; then
-  echo "pre-commit tool defined."
-else
-  input_params="use_pre_commit=yes ${input_params}"
-fi
 
 ENV_NAME=osl-python-package
 
-OUTPUT_DIR="/tmp/osl"
-rm -rf "${OUTPUT_DIR}"
-mkdir -p "${OUTPUT_DIR}"
-
-cookiecutter --no-input \
-  --output-dir "${OUTPUT_DIR}" \
-  "${PROJECT_PATH}/src/scicookie" \
-  ${input_params}
-
 cd "${OUTPUT_DIR}/${ENV_NAME}"
-
 
 if [[ "${input_params}" == *"use_conda=yes"* ]]; then
   set +x
@@ -62,10 +20,9 @@ if [[ "${input_params}" == *"use_conda=yes"* ]]; then
 fi
 
 if [[ "$input_params" == *"use_pyenv=yes"* ]]; then
-  set +x
   virtualenv "${ENV_NAME}"
   source "${ENV_NAME}/bin/activate"
-  set -x
+  pip install -r requirements.txt
 fi
 
 # remove any path to scicookie environment
@@ -127,3 +84,5 @@ export PATH=${PATH_ORI}
 
 echo '---------------------------- passed --------------------------'
 cd "${PWD_ORI}"
+
+set +ex
--- tests/smoke/virtual-envs.sh
+++ tests/smoke/virtual-envs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
 . ${SMOKE_DIR}/base.sh "use_conda=yes"
+. ${SMOKE_DIR}/base.sh "use_pyenv=yes"
+
+set +ex
