5adf87fc0bc9dbcf8eda98d42708e30f75cacd88
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2024-10-26 21:53:12 -0400

    refactor: Split build system config by tool

--- tests/smoke/ci.sh
+++ tests/smoke/ci.sh
@@ -2,6 +2,7 @@
 set -e
 
 SMOKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CHECK_SCHEMA_CMD="python ${SMOKE_DIR}/../scripts/check_schema.py"
 
 # Run the complete smoke tests
 . ${SMOKE_DIR}/base.sh "use_circleci=yes use_conda=yes"
@@ -24,20 +25,20 @@ popd
 
 . ${SMOKE_DIR}/base-template.sh "use_azure_pipelines=yes use_conda=yes"
 pushd /tmp/osl/osl-python-package
-  python "${SMOKE_DIR}/../scripts/check_schema.py" ${SMOKE_DIR}/schemas/azure-pipelines.json azure-pipelines.yml
+  ${CHECK_SCHEMA_CMD} ${SMOKE_DIR}/schemas/azure-pipelines.json azure-pipelines.yml
 popd
 
 . ${SMOKE_DIR}/base-template.sh "use_azure_pipelines=yes use_pyenv=yes"
 pushd /tmp/osl/osl-python-package
-  python "${SMOKE_DIR}/../scripts/check_schema.py" ${SMOKE_DIR}/schemas/azure-pipelines.json azure-pipelines.yml
+  ${CHECK_SCHEMA_CMD} ${SMOKE_DIR}/schemas/azure-pipelines.json azure-pipelines.yml
 popd
 
 . ${SMOKE_DIR}/base-template.sh "use_gitlab_ci=yes use_conda=yes"
 pushd /tmp/osl/osl-python-package
-  python "${SMOKE_DIR}/../scripts/check_schema.py" ${SMOKE_DIR}/schemas/gitlab-ci.json .gitlab-ci.yml
+  ${CHECK_SCHEMA_CMD} ${SMOKE_DIR}/schemas/gitlab-ci.json .gitlab-ci.yml
 popd
 
 . ${SMOKE_DIR}/base-template.sh "use_gitlab_ci=yes use_pyenv=yes"
 pushd /tmp/osl/osl-python-package
-  python "${SMOKE_DIR}/../scripts/check_schema.py" ${SMOKE_DIR}/schemas/gitlab-ci.json .gitlab-ci.yml
+  ${CHECK_SCHEMA_CMD} ${SMOKE_DIR}/schemas/gitlab-ci.json .gitlab-ci.yml
 popd
